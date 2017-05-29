5aa3141e8fb013c831f364d2d96cb70182ed7b1f
Stepan Sychev <ssychev@luxoft.com>
2017-05-29 10:37:46 +0300

    Добавлены и описаны все методы в сервисе

--- NetworkDictionary.Service/Controllers/NetworkDictionaryController.cs
+++ NetworkDictionary.Service/Controllers/NetworkDictionaryController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Net;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using NetworkDictionary.Dispatcher.Interfaces;
@@ -31,6 +29,120 @@ namespace NetworkDictionary.Service.Controllers
                 return new BadRequestObjectResult(ModelState);
 
             var result = await _dispatcher.GetKeys(request);
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Set value
+        /// </summary>
+        /// <response code="200">Execution success</response>
+        /// <response code="400">Invalid request</response>
+        /// <response code="500">Internal service error</response>
+        [HttpPost("key")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(ModelStateDictionary), 400)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> SetValue([FromBody]SetValueRequestDto request)
+        {
+            if (!ModelState.IsValid)
+                return new BadRequestObjectResult(ModelState);
+
+            await _dispatcher.SetValue(request);
+            return Ok();
+        }
+
+        /// <summary>
+        /// Get value
+        /// </summary>
+        /// <response code="200">Execution success</response>
+        /// <response code="400">Invalid request</response>
+        /// <response code="500">Internal service error</response>
+        [HttpGet("key")]
+        [ProducesResponseType(typeof(GetValueResponseDto), 200)]
+        [ProducesResponseType(typeof(ModelStateDictionary), 400)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> GetValue(GetValueRequestDto request)
+        {
+            if (!ModelState.IsValid)
+                return new BadRequestObjectResult(ModelState);
+
+            var result = await _dispatcher.GetValue(request);
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Delete Key
+        /// </summary>
+        /// <response code="200">Execution success</response>
+        /// <response code="400">Invalid request</response>
+        /// <response code="500">Internal service error</response>
+        [HttpDelete("key")]
+        [ProducesResponseType(typeof(DeleteKeyResponseDto), 200)]
+        [ProducesResponseType(typeof(ModelStateDictionary), 400)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> DeleteKey([FromBody]DeleteKeyRequestDto request)
+        {
+            if (!ModelState.IsValid)
+                return new BadRequestObjectResult(ModelState);
+
+            var result = await _dispatcher.DeleteKey(request);
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Get options
+        /// </summary>
+        /// <response code="200">Execution success</response>
+        /// <response code="400">Invalid request</response>
+        /// <response code="500">Internal service error</response>
+        [HttpGet("options")]
+        [ProducesResponseType(typeof(GetOptionsResponseDto), 200)]
+        [ProducesResponseType(typeof(ModelStateDictionary), 400)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> GetOptions()
+        {
+            if (!ModelState.IsValid)
+                return new BadRequestObjectResult(ModelState);
+
+            var result = await _dispatcher.GetOptions();
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Set options
+        /// </summary>
+        /// <response code="200">Execution success</response>
+        /// <response code="400">Invalid request</response>
+        /// <response code="500">Internal service error</response>
+        [HttpPost("options")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(ModelStateDictionary), 400)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> SetOptions([FromBody]SetOptionsRequestDto request)
+        {
+            if (!ModelState.IsValid)
+                return new BadRequestObjectResult(ModelState);
+
+            await _dispatcher.SetOptions(request);
+            return Ok();
+        }
+
+        /// <summary>
+        /// Packet execution
+        /// </summary>
+        /// <response code="200">Execution success</response>
+        /// <response code="400">Invalid request</response>
+        /// <response code="500">Internal service error</response>
+        [HttpPost("execute")]
+        [ProducesResponseType(typeof(PacketResponseDto), 200)]
+        [ProducesResponseType(typeof(ModelStateDictionary), 400)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> PacketExecution([FromBody]PacketRequestDto request)
+        {
+            if (!ModelState.IsValid)
+                return new BadRequestObjectResult(ModelState);
+
+            var result = await _dispatcher.GetPacketExecutionResult(request);
             return Ok(result);
         }
     }
